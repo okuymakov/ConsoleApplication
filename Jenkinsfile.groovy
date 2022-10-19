e34d245d9efe0f3edf05bf40749c48f18317e931
Oleg Kuymakov <96371600+okuymakov@users.noreply.github.com>
2022-10-19 18:13:42 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,8 +16,16 @@ pipeline {
         stage('backup') {
             steps {
                 bat 'Publish.cmd'
-                zip zipFile: 'CalcApp.zip', archive: false, dir: 'CalculationApp/bin/Release/netcoreapp3.1/publish'
-                archiveArtifacts artifacts: 'CalcApp.zip', fingerprint: true           
+                zip zipFile: '"CalcApp ${env.BUILD_NUMBER}".zip', archive: false, dir: 'CalculationApp/bin/Release/netcoreapp3.1/publish'
+                archiveArtifacts artifacts: 'CalcApp.zip', fingerprint: true, onlyIfSuccessful: true           
+            }
+        }
+        
+        stage('deploy') {
+            steps {
+                def path = 'C:/Deploy'
+                bat 'xcopy /Y /s "CalcApp ${env.BUILD_NUMBER}".zip ${path} /D'
+                unzip zipFile: '${path}/"CalcApp ${env.BUILD_NUMBER}".zip', dir: ${path}                       
             }
         }
     }
