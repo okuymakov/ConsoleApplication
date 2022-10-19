de70932d8bd721e81956075d55289029c4cb680c
Oleg Kuymakov <96371600+okuymakov@users.noreply.github.com>
2022-10-19 16:56:12 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,5 +12,12 @@ pipeline {
                 bat 'RunTests.cmd'
             }
         }
+        
+        stage('backup') {
+            steps {
+                bat 'Publish.cmd'
+                archiveArtifacts allowEmptyArchive: true, artifacts: 'CalcApp/bin/Release/netcoreapp3.1/publish/**', caseSensitive: false, followSymlinks: false, onlyIfSuccessful: true
+            }
+        }
     }
 }
